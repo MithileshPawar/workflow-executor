5cd13c191130712dc57bd5e55fef493fd55259bc
Mithilesh Pawar <150197362+MithileshPawar@users.noreply.github.com>
2026-01-21 21:38:31 -0500

    Initialized project. (#1)

---
+++ src/test/java/com/publicis/workflow_executor/WorkflowExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicis.workflow_executor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
