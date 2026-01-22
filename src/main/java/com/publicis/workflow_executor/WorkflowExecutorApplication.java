5e633f882a7573073570158c4c3e997288992332
Mithilesh Pawar <mithilesh10@gmail.com>
2026-01-21 21:36:39 -0500

    Initialized project.

---
+++ src/main/java/com/publicis/workflow_executor/WorkflowExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.publicis.workflow_executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowExecutorApplication.class, args);
+	}
+
+}
